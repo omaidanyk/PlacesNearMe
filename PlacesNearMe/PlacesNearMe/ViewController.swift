91f7532afb63ab2c741ad1b2b34ed7156b405a81
Oleksii Maidanyk <omaid@softserveinc.com>
2020-07-03 19:38:25 +0300

    initial commit

---
+++ PlacesNearMe/PlacesNearMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacesNearMe
+//
+//  Created by Oleksii Maidanyk on 03.07.2020.
+//  Copyright © 2020 omaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
